b75fd684f30d640f761b6566b7a38cd71237e76d
Lianmin Zheng <lianminzheng@gmail.com>
2023-02-03 05:31:20 +0000

    Archive 2023-02-03 05:31:18

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.2.dev39',
+    VERSION: '0.2.2.dev40',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
